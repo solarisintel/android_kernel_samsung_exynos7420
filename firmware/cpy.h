d4afc4fa52051ecab543248c552116e5cc21a3e3
solarisintel <solarisintel@yahoo.co.jp>
2026-01-09 15:08:55 +0900

    first commit

---
+++ firmware/cpy.h
@@ -0,0 +1,6 @@
+cp ~/sc05g/5.0/kernel/firmware/ssp_crashed.*               ./firmware
+cp ~/sc05g/5.0/kernel/firmware/tsp_stm/stm_tb_integ.*      ./firmware/tsp_stm/
+cp ~/sc05g/5.0/kernel/firmware/tsp_stm/stm_z1.*            ./firmware/tsp_stm/
+cp ~/sc05g/5.0/kernel/firmware/tsp_stm/stm_z2a.*           ./firmware/tsp_stm/
+cp ~/sc05g/5.0/kernel/firmware/tsp_stm/stm_z2i.*           ./firmware/tsp_stm/
+cp ~/sc05g/5.0/kernel/firmware/ssp_stm.*                   ./firmware
